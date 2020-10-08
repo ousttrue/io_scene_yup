a30e6d0769d50f1c92728d39ec21658f0793d5ce
ousttrue <ousttrue@gmail.com>
2020-10-08 19:26:02 +0900

    env

--- tests/from_bpy.py
+++ tests/from_bpy.py
@@ -1,11 +1,5 @@
 import bpy
 import bpy.utils
-import pathlib
-import sys
-
-HERE = pathlib.Path(__file__).absolute().parent
-sys.path.append(HERE.parent.parent)
-
 import io_scene_yup
 io_scene_yup.register()
 
